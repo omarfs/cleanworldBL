b8aa8179a7508c0aa5ea434b22975a80dc68af8b
Omar Fonseca <omar.a.fonseca@gmail.com>
2016-03-11 11:55:48 -0800

    add home and contact pages

--- source/assets/javascripts/all.js
+++ source/assets/javascripts/all.js
@@ -1,14 +1,15 @@
 // Bower
 // =====
-//= require jquery/jquery.min.js
-//= require jqueryeasing/jquery.easing.min.js
-
+//= require "jquery/jquery.min.js"
+//= require "jqueryeasing/jquery.easing.min.js"
+//= require "gsap/uncompressed/TweenMax.js"
 //= require "smooth-scroll.js/smooth-scroll.min.js"
-//= require "gsap/minified/TweenMax.min.js"
 //= require "ScrollMagic/minified/ScrollMagic.min.js"
 //= require "ScrollMagic/minified/plugins/debug.addIndicators.min.js"
 //= require "ScrollMagic/minified/plugins/animation.gsap.min.js"
 //= require "simplbox/simplbox.min.js"
+//= require "flickity/flickity.pkgd.min.js"
+
 //= require_tree .
 
 //= require init
--- source/assets/javascripts/init.js
+++ source/assets/javascripts/init.js
@@ -1,4 +1,41 @@
-$(document).ready(function() {
-    $("#mySlider").royalSlider({
-    });  
-});
+var hasClass = function (elem, className){
+	return new RegExp(' ' + className + ' ').test(' ' + elem.className + ' ');
+}
+
+var addClass = function (elem, className){
+	if(!hasClass(elem, className)) {
+		elem.className += ' ' + className;
+	}
+}
+
+var removeClass = function(elem, className){
+	var newClass = ' ' + elem.className.replace(/[\t\r\n]/g,' ')+' ';
+	if(hasClass(elem, className)) {
+		while (newClass.indexOf(' ' + className + ' ') >= 0){
+			newClass = newClass.replace(' ' + className + ' ', ' ');
+		}
+		elem.className = newClass.replace(/^\s+|\s+$/g, '');
+	}
+}
+
+if ('querySelector' in document && 'addEventListener' in window){
+
+	var siteNav = document.querySelector('.site-nav'),
+			heroBottom = document.querySelector('.bottom-panel'),
+			heroTop = document.querySelector('.top-panel'),
+			bottomChildren = heroBottom.children,
+			topChildren = heroTop.children,
+		tl = new TimelineMax({
+			paused:true//,
+			//repeat:-1
+		});
+
+		console.log(heroTop);
+		console.log(topChildren);
+
+		tl.from(siteNav,0.75,{yPercent:-100,opacity:0,ease:Circ.easeOut})
+			.from(topChildren[0], 0.5, {xPercent:25,opacity:0},"-=0.25")
+			.from(bottomChildren[0], 0.8, {top:50, opacity:0}, "+=0.5")
+			.play();
+
+}
