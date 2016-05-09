d97d506505aa8418f729845c87696aac758d86d6
Omar Fonseca <omar.a.fonseca@gmail.com>
2016-05-09 15:15:08 -0700

    work on nav-bar animation

--- source/assets/javascripts/_mobile-menu.js
+++ source/assets/javascripts/_mobile-menu.js
@@ -6,15 +6,11 @@ var navController = {
   animate: {
     duration: 0.5,
     visible: {
-      display: 'block',
-      autoAlpha: 1,
-      //marginTop: -8,
-      height: 0
+      //properties to be animated
     },
     hidden: {
-      display: 'none',
-      autoAlpha: 0,
-      height: 0
+      //properties to be animated
+
     }
   },
 
@@ -28,28 +24,43 @@ var navController = {
 
   init: function(){
     this.bindUI();
+    this.timeline();
   },
 
   bindUI: function(){
     this.trigger.addEventListener('click', this.handleClick);
   },
 
+  timeline: function(){
+    navTl = new TimelineMax({paused:true});
+  },
+
   handleClick: function(e){
     var _nc = navController,
         trigger = this,
         navId = trigger.getAttribute(_nc.attrs.id),
-        xNav = document.getElementById(navId);
+        xNav = document.getElementById(navId),
+        navItem = document.getElementsByClassName('nav-link');
 
+        //CSSPlugin.defaultTransformPerspective = 800;
     e.preventDefault();
+    navTl
+    .to(xNav,_nc.animate.duration,{height:"56px",opacity:1,ease:Expo.easeOut})
+    .staggerFrom(".nav-link",_nc.animate.duration,{opacity:0,y:-50,delay:0.1},.1);
     xNav.classList.toggle(_nc.classes.active);
     _nc.animate.visible.height = xNav.offsetHeight;
 
     if(_nc.isNavOpen(xNav)){
       trigger.classList.add(_nc.classes.active);
-      TweenLite.fromTo(xNav, _nc.animate.duration, _nc.animate.hidden, _nc.animate.visible);
+      //TweenLite.fromTo(xNav, _nc.animate.duration, _nc.animate.hidden, _nc.animate.visible);
+      navTl.play();
+      console.log(navTl.progress());
     } else {
       trigger.classList.remove(_nc.classes.active);
-      TweenLite.fromTo(xNav, _nc.animate.duration, _nc.animate.visible, _nc.animate.hidden);
+      navTl.reverse();
+      //TweenLite.fromTo(xNav, _nc.animate.duration, _nc.animate.visible, _nc.animate.hidden);
+      //TweenMax.staggerTo(".nav-link",_nc.animate.duration,{opacity:0,y:-50,delay:0.1},.1)
+      //TweenMax.to(xNav,_nc.animate.duration,{height:0,opacity:0});
     }
   },
 
